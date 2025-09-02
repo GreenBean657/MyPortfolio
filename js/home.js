6b423887fa8564f2077dbb718ef53b20dc1eb1e3
Green Bean <greenbean@greencypher.tech>
2025-09-02 17:37:53 -0600

    Added contact + Home + Skills

--- js/home.js
+++ js/home.js
@@ -14,3 +14,21 @@ fetch('https://dummyjson.com/quotes/random')
         console.log(err);
         quote.innerHTML = `<p class="centered reduced-padding">"Creativity is just suppressed imitation" - Nathaniel Cruz</p>`;
     });
+
+
+var slideIndex = 1;
+let maxSlide = 2;
+setInterval(carousel, 4000)
+
+function carousel() {
+    var i;
+    var x = document.getElementById("slideshow");
+
+    slideIndex++;
+    if (slideIndex > maxSlide) {
+        slideIndex = 1;
+    }
+
+    x.innerHTML = `<img class="slideshow-content" src="assets/homeslideshow/${slideIndex}.png" style="width:100%" alt="Slideshow of HIP Information">`;}
+
+carousel();
